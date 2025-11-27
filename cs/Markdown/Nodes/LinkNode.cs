8d840d190f80dd33e1d85bb1614c9524a58c7de3
shept <sheptikhinv@gmail.com>
2025-11-27 19:50:07 +0500

    add children to LinkNode.cs

--- cs/Markdown/Nodes/LinkNode.cs
+++ cs/Markdown/Nodes/LinkNode.cs
@@ -3,7 +3,8 @@ namespace Markdown.Nodes;
 public class LinkNode : MarkdownNode
 {
     public string Url { get; set; }
-    public string Label { get; set; }
+    public List<MarkdownNode> Children { get; set; } = [];
 
-    public override string ToHtml() => $"<a href=\"{Url}\">{Label}</a>";
+    public override string ToHtml() =>
+        $"<a href=\"{Url}\">{string.Concat(Children.Select(c => c.ToHtml()))}</a>";
 }
